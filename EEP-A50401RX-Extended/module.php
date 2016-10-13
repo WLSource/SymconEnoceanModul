acb1ade903ec0e7adc0ed8d378d7a51b33090127
WLSource <werner.leupold@gmx.de>
2016-10-13 15:01:33 +0200

    Calculations for registered variables added.

--- EEP-A50401RX-Extended/module.php
+++ EEP-A50401RX-Extended/module.php
@@ -15,6 +15,8 @@
 			$this->RegisterVariableFloat("HUM", "Humidity", "", 0);
 			$this->RegisterVariableFloat("TMP", "Temperature", "", 0);
 			$this->RegisterVariableFloat("VLT", "Battery", "", 0);
+			$this->RegisterVariableFloat("AHUM", "Abs. Humidity", "", 0);
+			$this->RegisterVariableFloat("DEW", "Dewpoint", "", 0);
 			
 			//Connect to available enocean gateway
 			$this->ConnectParent("{A52FEFE9-7858-4B8E-A96E-26E15CB944F7}");
@@ -35,17 +37,54 @@
 		public function ReceiveData($JSONString)
 		{
 			$data = json_decode($JSONString);
-			//IPS_LogMessage("IOTest", utf8_decode($data));
 			
-			
-			IPS_LogMessage("EnoceanGatewayData", $JSONString);
 			IPS_LogMessage("EnoceanGatewayData",  $data->{'DeviceID'});
-      			//Parse and write values to our variables
 			//$this->ParseData($JSONString);
 			$this->SetValueFloat("TMP", $data->{'DataByte1'});
 			$this->SendDebug("EnoceanGatewayData", $JSONString, 0);
+			$this->CalcProcessValues($data);
 		}
-    
+		
+		private function CalcProcessValues($spezData)
+		{
+			// temperatue = tempValue / 250 * 40 °C
+			$temperature = floatval($spezData->{'DataByte1'}); 
+			$temperature = $temperatue / 250 * 40;
+			
+			// humidity = humValue / 250 * 100 %
+			$humidity = floatval($spezData->{'DataByte2'}); 
+			$humidity = $humidity / 250 * 100;
+			
+			// goldCapVoltage = voltageValue / 255 * 1,8V * 4
+			$goldCapVoltage = floatval($spezData->{'DataByte3'});
+			$goldCapVoltage = $goldCapVoltage / 255 * 1.8 * 4;
+			
+			// Calc dewpoint and abs. humidity with Magnus coefficients
+			$c1 = 6.1078; 							// hPa
+			$c2 = 17.08085;                  // °C
+			$c3 = 234.175;                   // °C
+			$mw = 18.016;                    // g/mol
+			$uniGaskonstante = 8.3144598;    // J/(mol*K)
+			$tempInK = $temperature + 273.15;
+			// Sättigungsdampfdruck in hPa
+			$saettigungsDampfdruck = $c1 * exp(($c2 * $temperature) / ($c3 + $temperature));
+			$dampfdruck = $saettigungsDampfdruck *  $humidity / 100;
+			$taupunkt = (log($dampfdruck / $c1) * $c3) / ($c2 - log($saettigungsDampfdruck / $c1));
+			$absHum = $mw / $uniGaskonstante * $saettigungsDampfdruck / $tempInK * 100;
+			
+			// Write calculated values to registered variables
+			$this->SetValueFloat("TMP", $temperature);
+			$this->SetValueFloat("HUM", $humidity);
+			$this->SetValueFloat("VLT", $goldCapVoltage);
+			$this->SetValueFloat("AHUM", $absHum);
+			$this->SetValueFloat("DEW", $taupunkt);
+		}
+		
+		private function CalcDewpoint($spezTemperature, $spezRelHumidity)
+		{
+
+		}
+	 
 		private function ParseData($spezData)
 		{
 		// $searchPattern2 = '55 00 0a 07 01 eb a5';
